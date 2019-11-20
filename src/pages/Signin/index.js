163788bc2a90c42bfd1bfbb4273c8a03edfb8408
Flavio Kreis <flaviokreis@gmail.com>
2019-11-20 14:11:01 -0300

    Add auth redux, saga, logic and update layout
    Implement all logic of auth user and configure reactotron.

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,31 +1,52 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import { Wrapper, Content } from './styles';
 
 import logo from '../../assets/logo_login.svg';
 
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string().required('A senha é obrigatório'),
+});
+
 export default function Signin() {
+    const dispatch = useDispatch();
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit({ email, password }) {
+        dispatch(signInRequest(email, password));
+    }
+
     return (
         <Wrapper>
             <Content>
                 <img src={logo} alt="Gympoint" />
-                <form>
+                <Form schema={schema} onSubmit={handleSubmit}>
                     <strong>SEU E-MAIL</strong>
-                    <input
+                    <Input
                         name="email"
                         type="email"
                         placeholder="exemplo@email.com"
                     />
 
                     <strong>SUA SENHA</strong>
-                    <input
+                    <Input
                         name="password"
                         type="password"
                         placeholder="********"
                     />
 
-                    <button type="submit">Entrar no sistema</button>
-                </form>
+                    <button type="submit">
+                        {loading ? 'Carregando...' : 'Entrar no sistema'}
+                    </button>
+                </Form>
             </Content>
         </Wrapper>
     );
